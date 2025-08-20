cc5a416f60b1f7289859a2b17f83793e2a461b6a
Arjun <arjunsah856@gmail.com>
2025-08-20 21:03:46 +0545

    show result functionality added

--- script.js
+++ script.js
@@ -5,5 +5,36 @@ document.getElementById("ageForm").addEventListener("submit", function() {
     let month = parseInt(document.getElementById("month").value)
     let year = parseInt(document.getElementById("year").value)
 
+    // input validation
+    if(!day || !month || !year){
+        document.getElementById("result").innerText = "⚠️ Please enter a valid date!";
+        return;
+    }
+
+    let today = new Date();
+    let birthDate = new Date(year, month-1, day);
     
+    if(birthDate > today){
+        document.getElementById("result").innerText = "⚠️ Birth date cannot be in future!";
+    }
+
+
+    let ageYears = today.getFullYear() - birthDate.getFullYear()
+    let ageMonths = today.getMonth() - birthDate.getMonth()
+    let ageDays = today.getDay() - birthDate.getDate()
+
+    // adjust months and days
+    if(ageDays < 0){
+        ageMonths--
+        let prevMonth = new Date(today.getFullYear(), today.getMonth(), 0)
+        ageDays += prevMonth.getDate()
+    }
+
+    if(month < 0){
+        ageYears--
+        ageMonths += 12
+    }
+
+    document.getElementById("result"),this.innerText = `🎉 You are ${ageYears} years, ${ageMonths} months and ${ageDays} days old.`
+
 })
