d7f69b938a35eee4fc497a5d610f1a7668008cf3
Arjun <arjunsah856@gmail.com>
2025-08-20 14:57:46 +0545

    store input data through JS

---
+++ script.js
@@ -0,0 +1,9 @@
+document.getElementById("ageForm").addEventListener("submit", function() {
+    e.preventDefault();
+
+    let day = parseInt(document.getElementById("day").value)
+    let month = parseInt(document.getElementById("month").value)
+    let year = parseInt(document.getElementById("year").value)
+
+    
+})
